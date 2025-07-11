12d4e51c7c8894e7fca4149ede177c17c85cc02c
Tanish <tanish.iaf1852005@gmail.com>
2025-07-11 05:43:25 +0000

    changed-code-files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-background/95 py-4">
+    <header className="bg-[#fefffe] py-4">
       <div className="container flex h-auto max-w-screen-2xl flex-col items-center justify-center text-center">
         <Link href="/" className="flex flex-col items-center gap-2">
            <Image
