a498436637a9629d8652747dac8dc897f29ea9ea
Tanish <tanish.iaf1852005@gmail.com>
2025-08-28 13:44:22 +0000

    changed-code-files

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -90,7 +90,7 @@ export default function TrainingPage() {
 
         <div className="text-center">
             <p className="text-xl font-bold text-primary">
-                Kindly visit <Link href="/gallery/training-programmes" className="underline text-accent-foreground hover:text-accent transition-colors">Gallery</Link> for all activities of training.
+                Visit our <Link href="/gallery/training-programmes" className="underline text-accent-foreground hover:text-accent transition-colors">Gallery</Link> to view all training activities conducted.
             </p>
         </div>
 
